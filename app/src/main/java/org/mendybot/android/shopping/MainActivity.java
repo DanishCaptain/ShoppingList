8abe71157a5b7f81bd7c46c0dfd29376b119a9c9
Danish Captain <brian@bombholtmagic.com>
2019-04-07 19:12:57 -0700

    initial

---
+++ app/src/main/java/org/mendybot/android/shopping/MainActivity.java
@@ -0,0 +1,13 @@
+package org.mendybot.android.shopping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
